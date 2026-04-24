24555448ca1111b10e503cce32216792805535b4
Milton Rodrigues <milton.rodrigues@itec.al.gov.br>
2026-04-24 12:39:22 -0300

    Added Book model with the admin site

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Author
+from .models import Author, Book
 
 # Register your models here.
 admin.site.register(Author) # Register the Author model with the admin site
+admin.site.register(Book) # Register the Book model with the admin site
--- core/models.py
+++ core/models.py
@@ -4,6 +4,23 @@ from django.db import models
 class Author(models.Model):
     first_name = models.CharField(max_length=70, null=True, blank=True)
     last_name = models.CharField(max_length=70, null=True, blank=True)
+    title = models.CharField(max_length=10, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.title} {self.first_name} {self.last_name}"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        Author, 
+        on_delete=models.CASCADE,
+        related_name='books'
+    )
+    isbn = models.CharField(max_length=13, unique=True)
+    publication_date = models.DateField()
+    pages = models.IntegerField()
+    description = models.TextField(blank=True)
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
